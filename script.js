b375f67100a0f29d2f90eea45e1d7ffd5d4412f7
Swastik Kulshreshtha <56737344+Swastik2442@users.noreply.github.com>
2025-01-26 20:54:47 +0530

    Minor Fixes

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ const week = day * 7;
 const month = day * 30;
 const year = day * 365;
 
-function rgbToCSS(rgb) {
-    var customColor = rgb.split(" ");
-    customColor = customColor.map((c) => Math.ceil(c * 255));
+const rgbToCSS = (rgb) => {
+    const customColor = rgb.split(" ").map((c) => Math.ceil(c * 255));
     return "rgb(" + customColor + ")";
 }
 
-var wpProperties = {
+const wpProperties = {
     eventDate: "12/31/2099 23:59:59",
     eventName: "Special Event",
     timeEndText: "No Time Left !",
@@ -27,12 +26,11 @@ var wpProperties = {
     showZeroes: false,
 };
 
-var timeUntilDate = new Date(wpProperties.eventDate).getTime();
-var timeUntilElement = document.getElementById("timeUntil");
-var untilEventName = document.getElementById("untilEventName");
-var untilEventDate = document.getElementById("untilEventDate");
-var animationElement = document.getElementById("tiles");
-var animationTiles = animationElement.childNodes;
+const timeUntilElement = document.getElementById("timeUntil");
+const untilEventName = document.getElementById("untilEventName");
+const untilEventDate = document.getElementById("untilEventDate");
+const animationElement = document.getElementById("tiles");
+let timeUntilDate = new Date(wpProperties.eventDate).getTime();
 
 window.wallpaperPropertyListener = {
     applyUserProperties: function (properties) {
@@ -49,12 +47,12 @@ window.wallpaperPropertyListener = {
         }
 
         if (properties.schemecolor) {
-            var backgroundColor = rgbToCSS(properties.schemecolor.value);
+            const backgroundColor = rgbToCSS(properties.schemecolor.value);
             document.documentElement.style.backgroundColor = backgroundColor;
             document.body.style.backgroundColor = backgroundColor;
         }
         if (properties.textcolor) {
-            var textColor = rgbToCSS(properties.textcolor.value);
+            const textColor = rgbToCSS(properties.textcolor.value);
             document.documentElement.style.color = textColor;
             document.body.style.color = textColor;
         }
@@ -74,58 +72,52 @@ window.wallpaperPropertyListener = {
 };
 
 setInterval(function () {
-    var timeLeft = timeUntilDate - new Date().getTime();
+    let timeLeft = timeUntilDate - new Date().getTime();
     if (timeLeft < 0) {
         timeUntilElement.innerText = wpProperties.timeEndText;
         return;
     }
 
-    var years = 0, months = 0, weeks = 0, days = 0, hours = 0, minutes = 0, seconds = 0;
+    let years = 0, months = 0, weeks = 0, days = 0, hours = 0, minutes = 0, seconds = 0;
     if (wpProperties.showYears) {
         years = Math.floor(timeLeft / year);
         timeLeft = timeLeft % year;
-    }
-    if (wpProperties.showMonths) {
+    } if (wpProperties.showMonths) {
         months = Math.floor(timeLeft / month);
         timeLeft = timeLeft % month;
-    }
-    if (wpProperties.showWeeks) {
+    } if (wpProperties.showWeeks) {
         weeks = Math.floor(timeLeft / week);
         timeLeft = timeLeft % week;
-    }
-    if (wpProperties.showDays) {
+    } if (wpProperties.showDays) {
         days = Math.floor(timeLeft / day);
         timeLeft = timeLeft % day;
-    }
-    if (wpProperties.showHours) {
+    } if (wpProperties.showHours) {
         hours = Math.floor(timeLeft / hour);
         timeLeft = timeLeft % hour;
-    }
-    if (wpProperties.showMinutes) {
+    } if (wpProperties.showMinutes) {
         minutes = Math.floor(timeLeft / minute);
         timeLeft = timeLeft % minute;
-    }
-    if (wpProperties.showSeconds) {
+    } if (wpProperties.showSeconds) {
         seconds = Math.floor(timeLeft / second);
     }
 
-    var timeUntilString = "";
+    let timeUntilString = "";
     if (wpProperties.showZeroes) {
-        if (wpProperties.showYears) timeUntilString += `${years} Year${years > 1 ? "s" : ""} `;
-        if (wpProperties.showMonths) timeUntilString += `${months} Month${months > 1 ? "s" : ""} `;
-        if (wpProperties.showWeeks) timeUntilString += `${weeks} Week${weeks > 1 ? "s" : ""} `;
-        if (wpProperties.showDays) timeUntilString += `${days} Day${days > 1 ? "s" : ""} `;
-        if (wpProperties.showHours) timeUntilString += `${hours} Hour${hours > 1 ? "s" : ""} `;
-        if (wpProperties.showMinutes) timeUntilString += `${minutes} Minute${minutes > 1 ? "s" : ""} `;
-        if (wpProperties.showSeconds) timeUntilString += `${seconds} Second${seconds > 1 ? "s" : ""}`;
+        if (wpProperties.showYears) timeUntilString += `${years} Year${years == 1 ? "" : "s"} `;
+        if (wpProperties.showMonths) timeUntilString += `${months} Month${months == 1 ? "" : "s"} `;
+        if (wpProperties.showWeeks) timeUntilString += `${weeks} Week${weeks == 1 ? "" : "s"} `;
+        if (wpProperties.showDays) timeUntilString += `${days} Day${days == 1 ? "" : "s"} `;
+        if (wpProperties.showHours) timeUntilString += `${hours} Hour${hours == 1 ? "" : "s"} `;
+        if (wpProperties.showMinutes) timeUntilString += `${minutes} Minute${minutes == 1 ? "" : "s"} `;
+        if (wpProperties.showSeconds) timeUntilString += `${seconds} Second${seconds == 1 ? "" : "s"}`;
     } else {
-        if (years > 0) timeUntilString += `${years} Year${years > 1 ? "s" : ""} `;
-        if (months > 0) timeUntilString += `${months} Month${months > 1 ? "s" : ""} `;
-        if (weeks > 0) timeUntilString += `${weeks} Week${weeks > 1 ? "s" : ""} `;
-        if (days > 0) timeUntilString += `${days} Day${days > 1 ? "s" : ""} `;
-        if (hours > 0) timeUntilString += `${hours} Hour${hours > 1 ? "s" : ""} `;
-        if (minutes > 0) timeUntilString += `${minutes} Minute${minutes > 1 ? "s" : ""} `;
-        if (seconds > 0) timeUntilString += `${seconds} Second${seconds > 1 ? "s" : ""}`;
+        if (years > 0) timeUntilString += `${years} Year${years == 1 ? "" : "s"} `;
+        if (months > 0) timeUntilString += `${months} Month${months == 1 ? "" : "s"} `;
+        if (weeks > 0) timeUntilString += `${weeks} Week${weeks == 1 ? "" : "s"} `;
+        if (days > 0) timeUntilString += `${days} Day${days == 1 ? "" : "s"} `;
+        if (hours > 0) timeUntilString += `${hours} Hour${hours == 1 ? "" : "s"} `;
+        if (minutes > 0) timeUntilString += `${minutes} Minute${minutes == 1 ? "" : "s"} `;
+        if (seconds > 0) timeUntilString += `${seconds} Second${seconds == 1 ? "" : "s"}`;
     }
     timeUntilElement.innerText = timeUntilString.trim();
 }, second);
